8261df8d9a59e41d5a8e4a7ced3025c2397a1b03
Danny Yen <danny.y.yen@gmail.com>
2019-05-22 20:30:57 +0300

    third commit

--- mediate.py
+++ mediate.py
@@ -1,1 +1,14 @@
-from app import app
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
